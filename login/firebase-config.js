307a04b3e0140533d37c3dd92d6b043d797c3492
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2025-10-29 08:47:53 +0900

    Create firebase-config.js

---
+++ login/firebase-config.js
@@ -0,0 +1,9 @@
+// Firebaseの設定情報（Firebase Consoleからコピー）
+export const firebaseConfig = {
+  apiKey: "（例）AIzaSyD...xxxx",
+  authDomain: "writing-papers.firebaseapp.com",
+  projectId: "writing-papers",
+  storageBucket: "writing-papers.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
