86dc3834e0fdfdcae1cf458fb6eccc885ae965a8
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2025-10-29 10:36:24 +0900

    Update firebase-config.js

--- login/firebase-config.js
+++ login/firebase-config.js
@@ -1,13 +1,9 @@
-
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyB64DJKPuX8MOhqV4fEBl6jwl1B5PjleRQ",
   authDomain: "writing-papers.firebaseapp.com",
   projectId: "writing-papers",
   storageBucket: "writing-papers.firebasestorage.app",
   messagingSenderId: "14120068197",
-  appId: "1:14120068197:web:699751e2e6cd58f7f6128c",
-  measurementId: "G-B43V1G22CV"
+  appId: "1:14120068197:web:699751e2e6cd58f7f6128c"
 };
