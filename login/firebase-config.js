fde48504f75e3f66bcd28b95511b3c61badf13c8
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2025-10-29 09:50:27 +0900

    Update firebase-config.js

--- login/firebase-config.js
+++ login/firebase-config.js
@@ -1,9 +1,9 @@
-// Firebaseの設定情報（Firebase Consoleからコピー）
-export const firebaseConfig = {
-  apiKey: "（例）AIzaSyD...xxxx",
+const firebaseConfig = {
+  apiKey: "AIzaSyB64DJKPuX8MOhqV4fEBl6jwl1B5PjleRQ",
   authDomain: "writing-papers.firebaseapp.com",
   projectId: "writing-papers",
-  storageBucket: "writing-papers.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  storageBucket: "writing-papers.firebasestorage.app",
+  messagingSenderId: "14120068197",
+  appId: "1:14120068197:web:699751e2e6cd58f7f6128c",
+  measurementId: "G-B43V1G22CV"
 };
