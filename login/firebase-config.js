ea28bf107c03afeaa06b47c224a708fe44536dba
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2025-10-29 12:48:37 +0900

    Update firebase-config.js

--- login/firebase-config.js
+++ login/firebase-config.js
@@ -1,9 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-  apiKey: "AIzaSyB64DJKPuX8MOhqV4fEBl6jwl1B5PjleRQ",
-  authDomain: "writing-papers.firebaseapp.com",
-  projectId: "writing-papers",
-  storageBucket: "writing-papers.firebasestorage.app",
-  messagingSenderId: "14120068197",
-  appId: "1:14120068197:web:699751e2e6cd58f7f6128c"
+// Firebase v10+ SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const auth = getAuth(app);
