4bb05723f8da1d1986e070e7b1aab3ad2e0344dc
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2025-10-31 13:04:36 +0900

    Update main.js

--- login/main.js
+++ login/main.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { firebaseConfig } from "./firebase-config.js";
+import { firebaseConfig } from "./firebase-config.js?v=33";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
